76796fa6885154a473ec590a6b7a41110de1f034
Keith O'Rourke <ryan_orourke@live.com>
2022-01-19 19:44:13 -0500

    added bcrypt to password write and read to prevent writing plaintext passwords to db

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('knex')(require('../knexfile').development)
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+require('dotenv').config()
+const {KEY} = process.env
 
 
 router.post('/signup', (req, res) => {
@@ -10,7 +15,7 @@ router.post('/signup', (req, res) => {
   .where({username: newUser.username})
   .then(data => {
     console.log(data)
-    if (data) {
+    if (data.length) {
       return res.status(400).send("username already exists")
     }
     if(!newUser.password) {
@@ -19,22 +24,34 @@ router.post('/signup', (req, res) => {
     if(!newUser.username) {
       return res.status(400).send("Username required for sign up")
     }
-  if(newUser.mode === 'basic') {
-    newUser = {...newUser, trackDifficulty: false, preferredMetric: "RPE", trackPercentageOfMax: false}
-  }
-  console.log(newUser)
-  knex('users')
-  .insert(newUser)
-  .then(data => {
-    return res.status(200).json(newUser)
-  })
-  .catch(err => {
-    return res.status(400).send(`Error creating account: ${err}`)
-  })
+    if(newUser.mode === 'basic') {
+      newUser.password = bcrypt.hashSync(newUser.password, 10)
+      newUser = {...newUser, trackDifficulty: false, preferredMetric: "RPE", trackPercentageOfMax: false}
+    }
+    console.log(newUser)
+    knex('users')
+    .insert(newUser)
+    .then(data => {
+      delete newUser.password
+      return res.status(200).json(newUser)
+    })
+    .catch(err => {
+      return res.status(400).send(`Error creating account: ${err}`)
+    })
   })
 })
 
 router.post('/login', (req, res) => {
+  const user = req.body
+  knex('users')
+  .where({username: user.username})
+  .then(data => {
+    if (!bcrypt.compareSync(user.password, data[0].password)) {
+      return res.status(400).send("Incorrect password")
+    }
+    const token = jwt.sign({userId: data.id}, KEY)
+    return res.status(200).json(token)
+  })
 
 })
 
