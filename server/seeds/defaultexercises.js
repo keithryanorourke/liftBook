32e1451ac0d92ae1ee73cbf62b5508ea9e5dc06f
Keith O'Rourke <ryan_orourke@live.com>
2022-01-24 15:24:25 -0500

    Created defaultexercises table and seeded with values for several movements

---
+++ server/seeds/defaultexercises.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('defaultexercises').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('defaultexercises').insert([
+        {name: "Squat", muscle: "Quadriceps, Glutes"},
+        {name: "Deadlift", muscle: "Back, Hamstrings"},
+        {name: "Bench Press", muscle: "Chest, Triceps, Shoulders"},
+        {name: "Pull Ups", muscle: "Back, Biceps"},
+        {name: "Push Ups", muscle: "Chest, Shoulders, Triceps"},
+        {name: "Shoulder Press", muscle: "Shoulders, Triceps"},
+        {name: "Lunges", muscle: "Quadriceps, Glutes, Hamstrings"},
+      ]);
+    });
+};
