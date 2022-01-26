7e5e7027cb76588c907499f84cb3d6f2875f9ec8
Keith O'Rourke <ryan_orourke@live.com>
2022-01-26 15:30:10 -0500

    Added get endpoint to WorkoutRoutes which gives a specific workout based on id as long as associated JWT user id matches the account that created the workout, returns 404 if the workout does not belong to the user account

--- client/src/pages/WorkoutPage/WorkoutPage.js
+++ client/src/pages/WorkoutPage/WorkoutPage.js
@@ -1,15 +1,37 @@
 import "./WorkoutPage.scss"
 import axios from "axios"
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import {NavLink, Navigate} from "react-router-dom"
 import {useParams} from "react-router";
 
-const WorkoutPage = () => {
+const WorkoutPage = ({token}) => {
   const location=useParams();
+  const {workoutId} = location
+  const [workout, setWorkout] = useState(null)
+  const [lifts, setLifts] = useState([])
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/workouts/${workoutId}`, { headers: 
+    {
+    Authorization: `Bearer: ${token}`
+    } 
+    })
+    .then(response => {
+      console.log(response.data)
+      setWorkout(response.data)
+    })
+    .catch(error => {
+      alert(`${error}.\nThe workout you are trying to access is not associated with your account! `)
+    })
+  }, [])
 
   console.log(location)
   return (
-    <h1>{location.workoutId}</h1>
+    <section className="workout">
+      <div className="workout__top-container">
+        <h2>{workout ? workout.name : "Loading..."}</h2>
+      </div>
+    </section>
   )
 }
 
