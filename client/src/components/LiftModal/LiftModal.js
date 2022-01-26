17b0041b5770ac6c5dcc813d7c74743357f38a90
Keith O'Rourke <ryan_orourke@live.com>
2022-01-26 18:37:24 -0500

    Added extremely basic form validation to LiftModal and connected it to post endpoint for liftRoutes

--- client/src/components/LiftModal/LiftModal.js
+++ client/src/components/LiftModal/LiftModal.js
@@ -3,6 +3,10 @@ import "./LiftModal.scss"
 import close from "../../assets/icons/clear_black_24dp.svg"
 
 const LiftModal = ({settings, exercises, addLiftHandler, setOpenLiftModal}) => {
+  const closeModal = (e) => {
+    e.preventDefault()
+    setOpenLiftModal(false)
+  }
 
   console.log(settings)
 
@@ -14,35 +18,35 @@ const LiftModal = ({settings, exercises, addLiftHandler, setOpenLiftModal}) => {
         <div className="add-lift__top-container">
           <div className="add-lift__empty"></div>
           <h2 className="add-lift__title">Add New Lift</h2>
-          <button onClick={() => setOpenLiftModal(false)} className="add-lift__close"><img src={close} alt="X icon" className="add-lift__x" /></button>
+          <button onClick={closeModal} className="add-lift__close"><img src={close} alt="X icon" className="add-lift__x" /></button>
         </div>
-        <form onClick={addLiftHandler} className="add-lift__form">
+        <form onSubmit={addLiftHandler} className="add-lift__form">
           <label htmlFor="" className="add-lift__label">Exercise:
             <select name="exercise" id="" className="add-lift__exercise-dropdown">
               {exercises.map(exercise => {
-                return <option key={exercise.id} value={exercise} className="add-lift__exercise-option">{exercise.name}</option>
+                return <option key={exercise.id} value={JSON.stringify(exercise)} className="add-lift__exercise-option">{exercise.name}</option>
               })}
             </select>
           </label>
           <label className="add-lift__label">Weight:
-            <input type="number" name="weight" placeholder="Leave blank for bodyweight" className="add-lift__input" />
+            <input type="number" step=".01" name="weight" placeholder="Leave blank for bodyweight" className="add-lift__input" />
           </label>
           <div className="add-lift__radio-container">
             <div className="add-lift__separator">
-              <input type="radio" id="lbs" defaultChecked value="lbs" name="weight-metric" className="add-lift__radio"/>
+              <input type="radio" id="lbs" defaultChecked value="lbs" name="weightMetric" className="add-lift__radio"/>
               <label htmlFor="lbs" className="add-lift__label add-lift__label--radio">lbs</label>
             </div>
             <div className="add-lift__separator">
-              <input type="radio" id="kg" value="kg" name="weight-metric" className="add-lift__radio"/>
+              <input type="radio" id="kg" value="kg" name="weightMetric" className="add-lift__radio"/>
               <label htmlFor="kg" className="add-lift__label add-lift__label--radio">kg</label>
             </div>
           </div>
           <label className="add-lift__label">Reps:
-            <input type="number" name="reps" className="add-lift__input" />
+            <input type="number" placeholder="Enter a whole number greater than 0" name="reps" className="add-lift__input" />
           </label>
           {settings.trackDifficulty ? 
           <label className="add-lift__label">{settings.preferredMetric}:
-            <input type="number" name={settings.preferredMetric} className="add-lift__input" />
+            <input type="number" step=".5" placeholder={settings.preferredMetric === "RPE" ? "Number between 1-10" : "Any non negative number"} name="difficulty" className="add-lift__input" />
           </label>
           : null
           }
@@ -53,7 +57,7 @@ const LiftModal = ({settings, exercises, addLiftHandler, setOpenLiftModal}) => {
           : null
           }
           <div className="add-lift__button-container">
-            <button onClick={() => setOpenLiftModal(false)} className="add-lift__button">Cancel</button>
+            <button onClick={closeModal} className="add-lift__button">Cancel</button>
             <button className="add-lift__button add-lift__button--submit">Save</button>
           </div>
         </form>
--- client/src/pages/WorkoutPage/WorkoutPage.js
+++ client/src/pages/WorkoutPage/WorkoutPage.js
@@ -16,6 +16,20 @@ const WorkoutPage = ({token}) => {
   const [userSettings, setUserSettings] = useState(null)
 
   useEffect(() => {
+    axios.get(`http://localhost:8080/account/settings`, { headers: 
+    {
+    Authorization: `Bearer: ${token}`
+    } 
+    })
+    .then(response => {
+      setUserSettings(response.data)
+    })
+    .catch(error =>{
+      alert(`${error}.\nUser settings not retrieved! You will now be logged out.`)
+      Cookie.remove('token')
+      Navigate('../login', {replace: true})
+    })
+
     axios.get(`http://localhost:8080/workouts/${workoutId}`, { headers: 
     {
     Authorization: `Bearer: ${token}`
@@ -23,36 +37,67 @@ const WorkoutPage = ({token}) => {
     })
     .then(response => {
       setWorkout(response.data)
-      axios.get(`http://localhost:8080/account/settings`, { headers: 
-      {
-      Authorization: `Bearer: ${token}`
-      } 
-      })
-      .then(response => {
-        setUserSettings(response.data)
-      })
-      .catch(error =>{
-        alert(`${error}.\nUser settings not retrieved! You will now be logged out.`)
-        Cookie.remove('token')
-        Navigate('../login', {replace: true})
-      })
-      axios.get(`http://localhost:8080/exercises/`, { headers: 
-      {
-      Authorization: `Bearer: ${token}`
-      } 
-      })
-      .then(response => {
-        console.log(response.data)
-        setExercises(response.data)
-      })
+      
     })
     .catch(error => {
       alert(`${error}.\nThe workout you are trying to access is not associated with your account! `)
     })
+
+  
+
+  axios.get(`http://localhost:8080/exercises/`, { headers: 
+    {
+    Authorization: `Bearer: ${token}`
+    } 
+    })
+    .then(response => {
+      console.log(response.data)
+      setExercises(response.data)
+    })
   }, [])
 
   const addLiftHandler = (e) => {
+    e.preventDefault()
+    let exit = false;
+    const exercise = JSON.parse(e.target.exercise.value)
+    const newLift = {
+      workout_id: workoutId,
+      reps: parseInt(e.target.reps.value),
+      exercise_id: exercise.id,
+      weight: 0,
+      measure: e.target.weightMetric.value,
+      difficulty: 0,
+      percentageOfMax: 0,
+      metric: userSettings.preferredMetric
+    }
 
+    if (e.target.weight.value > 0 && e.target.weight.value < 500) {
+      newLift.weight = parseInt(e.target.weight.value)
+    }
+
+    if(userSettings.trackDifficulty && e.target.difficulty.value) {
+      newLift.difficulty = parseFloat(e.target.difficulty.value)
+    }
+
+    if(!newLift.reps) {
+      alert("Please enter a positive whole number into the reps field!")
+      exit=true;
+    }
+    console.log(newLift)
+
+    if(!exit) {
+      axios.post(`http://localhost:8080/lifts`, newLift, { headers: 
+      {
+        Authorization: `Bearer: ${token}`
+      } 
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch (error => {
+      console.log(error)
+    })
+    }
   }
 
   return (
