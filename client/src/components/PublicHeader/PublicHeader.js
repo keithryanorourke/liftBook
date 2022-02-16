52c5031fa066732504932b42172ef0ff1dd78879
Keith O'Rourke <ryan_orourke@live.com>
2022-02-15 19:58:21 -0500

    Changed semantic tag of header from section to header

--- client/src/components/PublicHeader/PublicHeader.js
+++ client/src/components/PublicHeader/PublicHeader.js
@@ -2,9 +2,9 @@ import "./PublicHeader.scss"
 
 const PublicHeader = () => {
   return (
-    <section className="public-header">
+    <header className="public-header">
         <h1 className="public-header__title">lift<span className="public-header__title--pop">Book</span></h1>
-    </section>
+    </header>
   )
 }
 
