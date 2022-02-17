cdb5216d43901cd160c14a970861bdcb698774b1
Keith O'Rourke <ryan_orourke@live.com>
2022-02-16 20:28:52 -0500

    Changed checkHost to always match to localhost since api calls are made from localhost even in production

--- server/middleware/checkHost.js
+++ server/middleware/checkHost.js
@@ -4,7 +4,7 @@ const {NODE_ENV} = process.env
 
 const checkHost = (req, res, next) => {
   console.log(req.hostname, NODE_ENV)
-  const matchHost = NODE_ENV === 'development' ? 'localhost' : 'liftbook.ca'
+  const matchHost = 'localhost'
   if(req.hostname === matchHost) {
     next()
   } 
