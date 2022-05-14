d007b1cd7d4a4f1dc2b2387d1b2931b823386157
Keith O'Rourke <ryan_orourke@live.com>
2022-05-14 15:52:06 -0400

    Refactored liftRoutes

--- server/routes/liftRoutes.js
+++ server/routes/liftRoutes.js
@@ -1,29 +1,25 @@
-const express = require('express')
-const router = express.Router()
-const liftController = require('../controllers/liftController')
-const authorize = require ("../middleware/authorize").authorize;
+const express = require("express");
+const router = express.Router();
+const liftController = require("../controllers/liftController");
+const authorize = require("../middleware/authorize").authorize;
 
+const { postLift, putLift, getWorkoutLifts, getExerciseLifts, deleteLift } =
+	liftController;
 
-const {postLift,
-  putLift,
-  getWorkoutLifts,
-  getExerciseLifts,
-  deleteLift} = liftController
-
-// Post a new lift
-router.post("/", authorize, postLift)
-
-// Edit existing lift
-router.put("/", authorize, putLift)
+router
+	.route("/")
+	// Post a new lift
+	.post(authorize, postLift)
+	// Edit existing lift
+	.put(authorize, putLift);
 
 // Get ALL lifts contained in one workout
-router.get("/:workoutId", authorize, getWorkoutLifts)
+router.get("/:workoutId", authorize, getWorkoutLifts);
 
 // get ALL LIFTS for a specific exercise
-router.get("/byExercise/:exerciseId", authorize, getExerciseLifts)
+router.get("/byExercise/:exerciseId", authorize, getExerciseLifts);
 
 // Delete a single lift
-router.delete("/:liftId", authorize, deleteLift)
+router.delete("/:liftId", authorize, deleteLift);
 
-
-module.exports=router
+module.exports = router;
